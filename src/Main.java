9127878f7e4e6c8fd77ef59888f78aefe15b8675
Rasmus Adolfsson <rasmus.adolfsson@student.kyh.se>
2021-11-03 15:08:13 +0100

    sout

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Detta är EXPLORING branchen");
-        System.out.println("EXPLORING BRANCH");
+        System.out.println("EXPLORIMNG BRANCH");
         Casino casino = new Casino();
         casino.intro();
         casino.mainMenu();
