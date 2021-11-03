f551674cb7fcb236a1a873ca8918c518812077d1
Rasmus Adolfsson <rasmus.adolfsson@student.kyh.se>
2021-11-03 15:08:57 +0100

    trial sout

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Detta är EXPLORING branchen");
-        System.out.println("EXPLORING BRANCH");
+        System.out.println("Detta är TRIAL branchen");
+        System.out.println("TRIAL BRANCH");
         Casino casino = new Casino();
         casino.intro();
         casino.mainMenu();
