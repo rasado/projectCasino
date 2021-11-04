682952bb2334cabd239f6fb7db2b1b795406d7a4
Rasmus Adolfsson <rasmus.adolfsson@student.kyh.se>
2021-11-04 21:22:42 +0100

    soutchange

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Detta är TRIAL branchen");
-        System.out.println("TRIAL BRANCH");
+        System.out.println("Detta är final branchen");
+        System.out.println("final BRANCH");
         Casino casino = new Casino();
         casino.intro();
         casino.mainMenu();
