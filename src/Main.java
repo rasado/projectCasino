a43cb822b5bd1559166a70e3bd964b419c4fb123
Rasmus Adolfsson <rasmus.adolfsson@student.kyh.se>
2021-11-04 21:35:18 +0100

    removed souts

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Detta är final branchen");
-        System.out.println("final BRANCH");
         Casino casino = new Casino();
         casino.intro();
         casino.mainMenu();
