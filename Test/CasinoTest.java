e25bbe4eda7005ea86ad188ab885ad50a156a5ca
Rasmus Adolfsson <rasmus.adolfsson@student.kyh.se>
2021-11-04 22:08:23 +0100

    changed tests

--- Test/CasinoTest.java
+++ Test/CasinoTest.java
@@ -4,19 +4,21 @@ import static org.junit.jupiter.api.Assertions.*;
 class CasinoTest {
 
     @Test
-    void intro() {
-        int minAge;
-        minAge = 21;
-        assertEquals(21, minAge);
+    public void getName(){
+        Character character = new Character();
+        character.setName("Arne");
+        String result = character.getName();
+        assertEquals("Arne",result);
     }
 
     @Test
-    public void settingName(){
+    public void getAge(){
         Character character = new Character();
-        character.setName("Arne");
-        boolean isNameSet = character.setName("Arne");
-        assertFalse(isNameSet, "Rasmus");
+        character.setAge(21);
+        int result = character.getAge();
+        assertEquals(21,result);
     }
+
 
     //Jag är ledsen men jag lyckades inte med fler Tester. Jag slet mitt hår länge och väl men eftersom
     //nästan all min kod är inputbaserad så var det oerhört svårt.
